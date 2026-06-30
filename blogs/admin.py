d39d0ddfcfaee5132f8ee2c521e127461862c65b
Aryan12Yadav <aryanyadav0507wp@gmail.com>
2026-06-30 16:22:08 +0530

    staff/regular users to only access their own posts

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,14 +1,81 @@
 from django.contrib import admin
-from .models import Category,Blog,Comment
-# Register your models here.
+from .models import Category, Blog, Comment
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'author', 'created_at', 'updated_at')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.author != request.user:
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.author != request.user:
+            return False
+        return super().has_delete_permission(request, obj)
 
 class blogAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('title',)}
-    list_display = ('title','category','author','status','is_featured')
-    search_fields = ('id','title','category__category_name','status')
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
     list_editable = ('is_featured',)
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
 
-admin.site.register(Category)
-admin.site.register(Blog,blogAdmin)
-admin.site.register(Comment)
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.author != request.user:
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.author != request.user:
+            return False
+        return super().has_delete_permission(request, obj)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'user', 'blog', 'created_at')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.user != request.user:
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser and obj.user != request.user:
+            return False
+        return super().has_delete_permission(request, obj)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, blogAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     category_name   = models.CharField(max_length = 50,unique = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-
 
     class Meta:
         verbose_name_plural = 'categories'
